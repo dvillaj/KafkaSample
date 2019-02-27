324bf4f23362d9d04dabc1ae0c8a269577056ad9
Daniel <dvillaj@gmail.com>
2019-02-27 08:56:46 +0100

    Check if  kafka server is online

--- partitions.py
+++ partitions.py
@@ -1,19 +1,27 @@
 # encoding: utf-8
 
 from kafka import SimpleClient
+from kafka.errors import KafkaUnavailableError, NoBrokersAvailable
 from kafka.protocol.offset import OffsetRequest, OffsetResetStrategy
 from kafka.common import OffsetRequestPayload
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('topic', help="Tópico Kafka")
+parser.add_argument('--brokers', help="Broker en el formato dirección:puerto", default = "localhost:9092")
 
 args = parser.parse_args()
 
-brokers = 'localhost:9092'
+brokers = args.brokers
 topic = args.topic
 
-client = SimpleClient(brokers)
+try:
+    client = SimpleClient(brokers)
+except KafkaUnavailableError:
+    print("El servidor de Kafka no se encuentra disponible!")
+    sys.exit(0)
+
 
 partitions = client.topic_partitions[topic]
 offset_requests = [OffsetRequestPayload(topic, p, -1, 1) for p in partitions.keys()]
